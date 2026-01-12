94d938ec2e996709182e7bb2db6097eed2d59326
jimboss <salazar.dj100@gmail.com>
2026-01-12 11:00:43 -0500

    feat(survey-command): add base service infrastructure and health endpoint

---
+++ my-turborepo/apps/survey-command/src/main/java/com/asmas/surveycommand/SurveyCommandApplication.java
@@ -0,0 +1,12 @@
+package com.asmas.surveycommand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveyCommandApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SurveyCommandApplication.class, args);
+    }
+}
---
+++ my-turborepo/apps/survey-command/src/main/java/com/asmas/surveycommand/controller/SurveyController.java
@@ -0,0 +1,20 @@
+package com.asmas.surveycommand.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SurveyController {
+
+    @GetMapping("/protected/survey/ping")
+    public ResponseEntity<?> ping(
+            @RequestHeader("X-Username") String username,
+            @RequestHeader("X-User-Id") String userId
+    ) {
+        return ResponseEntity.ok(
+                "Authenticated request from user=" + username + ", id=" + userId
+        );
+    }
+}
