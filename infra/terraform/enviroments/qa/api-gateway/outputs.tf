514a116f854fea34c1167736001934273a4ae1ce
jimboss <salazar.dj100@gmail.com>
2026-01-23 12:49:23 -0500

    feat(api-gateway): add ALB configuration and outputs for DNS and ARN

--- infra/terraform/enviroments/qa/api-gateway/outputs.tf
+++ infra/terraform/enviroments/qa/api-gateway/outputs.tf
@@ -17,3 +17,15 @@ output "service_port" {
 output "ssh_command" {
   value = "ssh -i ~/.ssh/${var.key_name}.pem ubuntu@${aws_eip.this.public_ip}"
 }
+
+output "alb_dns_name" {
+  value = aws_lb.this.dns_name
+}
+
+output "alb_arn" {
+  value = aws_lb.this.arn
+}
+
+output "alb_target_group_arn" {
+  value = aws_lb_target_group.this.arn
+}
